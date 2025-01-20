2a24f0ff9e28b6d8728dd6fb251499dc120c8d12
Ondřej Tůma <ondrej.tuma.024@pslib.cz>
2025-01-20 10:35:26 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
-Sensors.SetLightLevel()
+input.onButtonPressed(Button.A, function () {
+    Sensors.SetLightLevel()
+    play()
+})
+
+let gameRunning = false
+let startTime = 0
+
+function play() {
+    basic.pause(randint(3000, 6000))
+    startTime = control.millis();
+    gameRunning = true
+    basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+}
+
+Sensors.OnLightDrop(function () {
+    console.log(control.millis() - startTime)
+    gameRunning = false
+    play()
+})
+
+basic.forever(function () {
+    //if (gameRunning = false && ) {
+    //    basic.showIcon(IconNames.Sad)
+    //}
+})
